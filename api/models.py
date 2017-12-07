6740f44facb80116956670335ece93a5c66f8b89
Farhan Ar Rafi <farhan.rafi@bjitgroup.com>
2017-12-07 15:05:35 +0600

    GEO_MIDDLEWARE: Added UserInfo and Location Data Save
    - Added column in app and user table to store generated token
    
    Refs - N/A

--- api/models.py
+++ api/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+
 class AppInfo(models.Model):
     app_name = models.CharField(max_length=250)
     app_email = models.CharField(max_length=250)
     app_type = models.CharField(max_length=250)
+    app_key = models.CharField(max_length=250)
+
 
 class UserInfo(models.Model):
     user_name = models.CharField(max_length=250)
     user_email = models.CharField(max_length=250)
     user_phone = models.CharField(max_length=20)
+    user_key = models.CharField(max_length=20)
+
+
+class LocationData(models.Model):
+    location_name = models.CharField(max_length=250)
+    location_type = models.CharField(max_length=250)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    altitude = models.DecimalField(max_digits=7, decimal_places=2)
+    app_key = models.CharField(max_length=250)
+    user_key = models.CharField(max_length=20)
--- api/views.py
+++ api/views.py
@@ -1,14 +1,17 @@
-import json
 import hashlib
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from api.models import AppInfo
 from api.models import UserInfo
+from api.models import LocationData
+
 
 def index(request):
-    return JsonResponse({'status':'OK'})
+    return JsonResponse({'status': 'OK'})
+
 
 @csrf_exempt
 def app(request):
@@ -18,25 +21,50 @@ def app(request):
         app_email = data["email"]
         app_type = data["type"]
         if app_name != "" and app_email != "" and app_type != "":
-            a_info = AppInfo(app_name=app_name,app_email=app_email,app_type=app_type)
+            token = hashlib.sha1("%sand%s" % (app_name, app_email)).hexdigest()
+            a_info = AppInfo(app_name=app_name, app_email=app_email, app_type=app_type, app_key=token)
             a_info.save()
-            token = hashlib.sha1("%sand%s"%(app_name,app_email)).hexdigest()
-            return JsonResponse({'token':token})
+            return JsonResponse({'token': token})
         else:
-            return JsonResponse({'status':'ERROR'})
+            return JsonResponse({'status': 'ERROR'})
     elif request.method == 'GET':
-        return JsonResponse({'status':'ERROR'})
+        return JsonResponse({'status': 'ERROR'})
+
 
 @csrf_exempt
 def user(request):
     if request.method == 'POST':
-        return JsonResponse({'status':'OK'})
+        data = json.loads(request.body.decode('utf-8'))
+        user_name = data["name"]
+        user_email = data["email"]
+        user_phone = data["phone"]
+        if user_name != "" and (user_email != "" or user_phone != ""):
+            token = hashlib.sha1("%sand%sand%s" % (user_name, user_email, user_phone)).hexdigest()
+            user_info = UserInfo(user_name=user_name, user_email=user_email, user_phone=user_phone, user_key=token)
+            user_info.save()
+            return JsonResponse({'token': token})
+        else:
+            return JsonResponse({'status': 'ERROR'})
     elif request.method == 'GET':
-        return JsonResponse({'status':'ERROR'})
+        return JsonResponse({'status': 'ERROR'})
+
 
 @csrf_exempt
 def data(request):
     if request.method == 'POST':
-        return JsonResponse({'status':'OK'})
+        data = json.loads(request.body.decode('utf-8'))
+        location_name = data["name"]
+        location_type = data["type"]
+        latitude = data["latitude"]
+        longitude = data["longitude"]
+        altitude = data["altitude"]
+        app_key = data["appKey"]
+        user_key = data["userKey"]
+        if latitude != "" and longitude != "" and app_key != "" and user_key != "":
+            location_data = LocationData(location_name=location_name, location_type=location_type,
+                                         latitude=latitude, longitude=longitude, altitude=altitude,
+                                         app_key=app_key, user_key=user_key)
+            location_data.save()
+            return JsonResponse({'status': 'OK'})
     elif request.method == 'GET':
-        return JsonResponse({'status':'ERROR'})
+        return JsonResponse({'status': 'ERROR'})
