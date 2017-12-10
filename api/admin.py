59081b97ad08ec5bc1ebc596c35f73e290321acd
Farhan <farhanarrafi@gmail.com>
2017-12-10 20:13:34 +0600

    GEO_MIDDLEWARE: Added ocation Data in admin panel
    - Added ocation Data in admin panel
    
    Refs - N/A

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import AppInfo
 from .models import UserInfo
+from .models import LocationData
+
 
 admin.site.register(AppInfo)
 admin.site.register(UserInfo)
+admin.site.register(LocationData)
